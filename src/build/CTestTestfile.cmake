4a952a736f4c7ffbc883a72f697168e8d1eabc97
artnr7_workarch <artnr7@gmail.com>
2025-11-29 16:23:19 +0300

    dont know what it is

--- src/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/projs/shape_detector/src
-# Build directory: /root/projs/shape_detector/src/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
